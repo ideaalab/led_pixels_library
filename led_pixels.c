c90c882f3106c74754c36fe3fc04505536ae58fe
Martin <martin@ideaaab.com>
2017-04-21 13:46:26 +0200

    Commit inicial
    Libreria funcional
    Por algun motivo la funcion "set_brightness" no parece funcionar bien

---
+++ led_pixels.c
@@ -0,0 +1,382 @@
+#include <led_pixels_v0.3.h>
+
+/*
+ * Inicializa la libreria:
+ *	-configura timer2 si es necesario
+ *	-Pone el pin de datos en LOW
+ *	-Limpia los pixels
+ */
+void InitPixels(void){
+#ifdef PIX_DELAY_TIMER2
+	//Configura timer para desbordar cada 50uS, pero no genera interrupcion
+	setup_timer_2(T2_DIV_BY_4, 99, 1);
+	disable_interrupts(INT_TIMER2);
+#endif
+	output_low(PIX_PIN);
+	LlenarDeColor(0, PIX_NUM_LEDS-1, PIX_NEGRO); //pone todos los pixels en negro
+}
+
+/*
+ * Pone un led en el color especificado
+ * El color debe ser pasado como una variable INT por cada color
+ */
+void SetPixelColor(int n, int r, int g, int b){
+	if(n < PIX_NUM_LEDS){
+		if(Brillo != 0){ //Mirar notas en SetBrightness()
+			r = ((long)r * Brillo) >> 8;
+			g = ((long)g * Brillo) >> 8;
+			b = ((long)b * Brillo) >> 8;
+		}
+		
+		int * p;
+		p = &Pixels[n * 3];
+		
+#ifdef PIX_RGB
+		//para led RGB
+		*p++ = r;
+		*p++ = g;
+		*p = b;
+#else
+		//para leds GRB
+		*p++ = g;
+		*p++ = r;
+		*p = b;
+#endif
+	}
+}
+
+/*
+ * Pone un led en el color especificado
+ * El color debe ser pasado como variable de 32bits
+ */
+void SetPixelColor(int n, int32 c){
+	if(n < PIX_NUM_LEDS){
+		int r = (int)(c >> 16);
+		int g = (int)(c >>  8);
+		int b = (int)c;
+		
+		if(Brillo != 0){ //Mirar notas en SetBrightness()
+			r = ((long)r * Brillo) >> 8;
+			g = ((long)g * Brillo) >> 8;
+			b = ((long)b * Brillo) >> 8;
+		}
+		
+		int * p;
+		p = &Pixels[n * 3];
+		
+#ifdef PIX_RGB
+		//para led RGB
+		*p++ = r;
+		*p++ = g;
+		*p = b;
+#else
+		//para leds GRB
+		*p++ = g;
+		*p++ = r;
+		*p = b;
+#endif
+	}
+}
+
+/*
+ * Convierte los colores separados R,G,B en un paquete de 32 bits con el color
+ */
+int32 Color32(int r, int g, int b){
+	return ((int32)r << 16) | ((int16)g <<  8) | b;
+}
+
+/*
+ * Devuelve el color del pixel "n" en formato de 32bits y en order RGB
+ */
+int32 GetPixelColor(int n){
+	if(n < PIX_NUM_LEDS) {
+		int ofs = n * 3;
+		
+#ifdef PIX_RGB
+		//para led RGB
+		return (int32)((Pixels[ofs] << 16) | (Pixels[ofs + 1] <<  8) | (Pixels[ofs + 2]));
+#else
+		//para leds GRB
+		return (int32)((Pixels[ofs + 1] <<  8) | (Pixels[ofs] << 16) | (Pixels[ofs + 2]));
+#endif
+		
+	}
+
+  return 0; //pixel fuera de limites
+}
+
+/*
+ * Ajusta el brillo de TODOS los leds
+ * 0 = completamente apagado, 255 = completamente encendido
+ * El brillo no se ve reflejado automaticamente en los leds
+ * La proxima llamada a MostrarPixels() actualizara el brillo
+ * 
+ * Esta funcion genera perdidas de resolucion al aumentar el brillo
+ * 
+ * No se puede actualizar el brillo al vuelo (mientras se envian los datos)
+ * por eso hay que hacerlo previamente diractamente sobre la RAM, lo que
+ * modifica permanentemente los datos de color
+ * 
+ * Si quisieramos modificar el brillo sin perdida de datos necesitariamos
+ * otro array con la informacion de todos los leds. En este array estaria
+ * la informacion de color sin modificar, y el array que se envia es el que
+ * tiene aplicados los ajustes de brillo. De esta manera los ajustes de brillo
+ * se harian teniendo en cuenta el array con los colores, pero no se
+ * sobreescribirian los valores reales
+ * 
+ * El valor de brillo almacenado es diferente al valor asignado. De esta manera
+ * se optimizan las instrucciones de escalado. La variable "brightness" es un INT.
+ * 0 = maximo brillo, 1 = minimo brillo (off), 255 = uno menos a maximo brillo.
+ */
+void CambiarBrillo(int b){
+	int NuevoBrillo = b + 1;
+	
+	//solo cambiamos el brillo si el nuevo valor es diferente al antiguo
+	if(NuevoBrillo != Brillo){
+		int Val;							//valor de color, solo es una parte del R/G/B
+		int *ptr = Pixels;					//puntero a el array de pixels
+		int BrilloAnterior = Brillo - 1;	//brillo anterior (real, de 0 a 255)
+		long Escala;						//escala, se multiplicn todos los valores por esta escala
+		
+		if(BrilloAnterior == 0)
+			Escala = 0;						//evitamos division entre 0
+		else if (b == 255)
+			Escala = 65535 / BrilloAnterior;
+		else 
+			Escala = (((long)NuevoBrillo << 8) - 1) / BrilloAnterior;
+		
+		for(int i=0; i<PIX_NUM_BYTES; i++) {
+			Val = *ptr;
+			*ptr = (Val * Escala) >> 8;
+			*ptr++;
+		}
+		
+		Brillo = NuevoBrillo;
+	}
+}
+
+/*
+ * Llena pixels de un mismo color, NO MUESTRA LOS CAMBIOS, hay que llamar a MostrarPixels
+ * -from: desde que pixel empezar (empezando desde 0)
+ * -to: hasta que pixel (empezando desde 0)
+ * -color: color en formato int32 RGB
+ */
+void LlenarDeColor(int from, int to, int32 c){
+int i;
+	
+	for(i = from; i<=to; i++){
+		SetPixelColor(i, c);
+	}
+}
+
+/*
+ * Envia el array de colores por el pin de datos
+ */
+void MostrarPixels(void){
+short GIEval;		//Valor de GIE
+int i;				//Loop
+
+	GIEVal = GIE;	//Guardo valor de global interrupt enable
+	GIE = 0;		//Deshabilito interrupciones
+	
+	i = PIX_NUM_BYTES;	//Numero de bytes a enviar
+	
+#ifdef PIX_DELAY_TIMER2
+	//Espero a que hayan transcurrido 50uS antes de volver a enviar
+	while(TMR2IF == FALSE){delay_cycles(1);}
+#endif
+
+	//Apunto FSR0 al inicio de mis bytes
+	FSR0L = Pixels;
+	FSR0H = Pixels >> 8;
+
+	//esta instruccion es para que el compilador se mueva al banco correcto antes de
+	//empezar con el envio de datos
+	output_low(PIX_PIN);
+
+#ifndef PIX_400KHZ
+//Envio de datos a 800Khz
+//10 instrucciones por cada bit: HHxxxxxLLL
+//OUT instructions:              ^ ^    ^   (T=0,2,7)
+
+SendByte1:					//Clk	Instr
+	//bit7 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 7))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit6 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 6))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit5 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 5))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit4 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 4))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit3 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 3))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit2 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 2))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit1 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 1))	//1		1
+		output_low(PIX_PIN);//2		1
+	delay_cycles(4);		//3-6	4
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8-9	2
+	
+	//bit0 ---
+	output_high(PIX_PIN);	//0		1
+	if(!bit_test(INDF0, 0))	//1		1
+		output_low(PIX_PIN);//2		1
+#asm
+	DECFSZ	i, F			//3		1	decrementar contador de bytes enviados, si es cero salta 1 -> listo.
+	GOTO	Salto1			//4		2	salta 1 instruccion (no me deja usar GOTO $+2 ni BRA 2 ¿?)
+	GOTO	Listo1			//5		2	todo enviado. Salir
+#endasm
+Salto1:
+	FSR0L++;				//6		1	incremento puntero
+	output_low(PIX_PIN);	//7		1	PIN = LOW
+	goto SendByte1;			//8		2	vuelve al principio
+
+Listo1:	
+	output_low(PIX_PIN);	//7		1
+	delay_cycles(2);		//8		2
+	//Fin de transmision
+	
+#else
+//Envio de datos a 400Khz
+//20 instrucciones por cada bit: HHHHxxxxxxLLLLLLLLLL
+//OUT instructions:              ^   ^     ^          (T=0,4,10)
+
+SendByte2:					//Clk	Instr
+	//bit7 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 7))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit6 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 6))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit5 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 5))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit4 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 4))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit3 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 3))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit2 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 2))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit1 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 1))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	delay_cycles(9);		//11-19	9
+	
+	//bit0 ---
+	output_high(PIX_PIN);	//0		1
+	delay_cycles(2);		//1-2	2
+	if(!bit_test(INDF0, 0))	//3		1
+		output_low(PIX_PIN);//4		1
+	delay_cycles(5);		//5-9	5
+	output_low(PIX_PIN);	//10	1
+	
+	//delay_cycles(9);		//11-19	9
+#asm
+	DECFSZ	i, F			//11	1	decrementar contador de bytes enviados, si es cero salta 1 -> listo.
+	GOTO	Salto2			//12	2	salta 1 instruccion (no me deja usar GOTO $+2 ni BRA 2 ¿?)
+	GOTO	Listo2			//13	2	todo enviado. Salir
+#endasm
+Salto2:
+	FSR0L++;				//14	1	incremento puntero
+	delay_cycles(3);		//15-17	3
+	goto SendByte2;			//18	2	vuelve al principio
+
+Listo2:	
+	delay_cycles(5);		//15-19	2
+	//Fin de transmision
+
+#endif	//Fin de envio de datos a 400Khz
+
+#ifdef PIX_DELAY_TIMER2
+	//La demora de 50uS se genera con el Timer2
+	TMR2 = 0;			//Reinicio contador de Timer2
+	TMR2IF = FALSE;		//Quito flag de interrupcion
+#else
+	//La demora de 50uS se genera con un delay
+	delay_us(50);		//espero 50uS para volver a enviar
+#endif
+
+	GIE = GIEval;		//restauro valor de GIE
+}
---
+++ led_pixels.h
@@ -0,0 +1,181 @@
+/* 
+ * File:   led_pixels_v0.3.h
+ * Author: Martin
+ *
+ * Created on 23 de enero de 2017, 20:52
+ */
+
+/* ------------------------------- INFORMACION --------------------------------
+ * 
+ *					WS2811 / WS2812 LED DRIVER
+ * 
+ * Libreria creada por Martin Andersen para IDEAA Lab el 14/abr/2014
+ * Usando compilador CCS v5.064
+ * 
+ * <http://www.martinandersen.es> / <http://www.ideaalab.com>
+ * 
+ * Informacion relevante sobre conexionado, funcionamiento y consejos en:
+ * <https://learn.adafruit.com/adafruit-neopixel-uberguide/overview>
+ * 
+ * Basada en libreria Neo_Pixel de Adafruit:
+ * <https://github.com/adafruit/Adafruit_NeoPixel>
+ * 
+ * Rutina de assembler creada con la ayuda del foro TodoPic:
+ * <http://www.todopic.com.ar/foros/index.php?topic=42611>
+ * 
+ * Esta libreria se distribuye como software libre. Se puede modificar
+ * y/o redistribuir bajo los terminos de licencia GNU:
+ * <http://www.gnu.org/licenses/gpl.html>
+ * ---------------------------------------------------------------------------*/
+
+/* -------------------------- INSTRUCCIONES DE USO ----------------------------
+ * 
+ * Definir el pin a usar con el nombre "PIX_PIN" como #bit, ejemplo:
+ * #define PIX_PIN	PIN_A0
+ * 
+ * Definir la cantidad de LEDs a usar con "PIX_NUM_LEDS":
+ * #define PIX_NUM_LEDS	20
+ * 
+ * Por defecto la libreria envia datos a 800Khz. Hay tiras de LED antiguas que no
+ * soportan el envio de datos a 800Khz. Para enviar a 400Khz definir "PIX_400KHZ":
+ * #define PIX_400KHZ
+ * 
+ * Depende del modelo de leds que estemos usando el orden de los colores
+ * puede ser RGB o GRB. Para los leds WS2812 el orden es GRB y es el
+ * funcionamiento predeterminado. Para leds RGB usar esto:
+ * #define PIX_RGB
+ * 
+ * Entre cada trama de datos deben transcurrir 50uS. Podemos usar un delay,
+ * que bloquea la ejecucion del programa o podemos usar el Timer 2 para poder
+ * seguir ejecutando instrucciones. Es recomendable usar el Timer 2 para generar
+ * el delay y no atascar el programa.
+ * La libreria por defecto usa el delay. Si queremos que use el Timer 2
+ * declarar la constante "PIX_DELAY_TIMER2":
+ * #define PIX_DELAY_TIMER2
+ * 
+ * Hay que usar FAST_IO para el puerto del pin de datos, si se usa STANDARD_IO
+ * se agrega una instruccion extra cada vez que se cambia el pin de estado
+ * y esto descontrola los tiempos de envio -> es decir, NO FUNCIONA!
+ * #use fast_io(a)
+ * ---------------------------------------------------------------------------*/
+
+/* --------------------------------- FUNCIONES --------------------------------
+ * 
+ * -InitPixels()
+ * Inicializa el array de leds
+ * 
+ * -SetPixelColor(int n, int r, int g, int b)
+ * Escribe en el LED (n) el color (r, g, b)
+ * 
+ * -SetPixelColor(int n, int32 c)
+ * Escribe en el LED (n) el color (c)
+ * 
+ * -Color32(int r, int g, int b)
+ * Se le pasan los colores separados y devuelve un color en formato int32
+ * 
+ * -GetPixelColor(int n)
+ * Devuelve el color que hayamos escrito previamente a un LED
+ * 
+ * -CambiarBrillo(int b)
+ * Permite ajustar el brillo de los LEDs sin cambiar su color
+ * (esta funcion genera perdida, mirar los comentarios en la funcion)
+ * 
+ * -LlenarDeColor(int from, int to, int32 c)
+ * Llena de un color (c) los pixels comprendidos entre (from) y (to)
+ * 
+ * -MostrarPixels()
+ * Envia la trama de datos a los LEDs. Cualquier otra operacion realizada
+ * solo modifica los datos de color >> en la RAM <<, pero hasta que no se utiliza
+ * MostrarPixels() no se enviara a los LEDs.
+ * CUIDADO se desactivan las interrupciones mientras se envian datos!
+ * ---------------------------------------------------------------------------*/
+
+/* --------------------------------- VERSIONES --------------------------------
+ * 
+ * v0.3 (23/Enero/2017)
+ * -Se divide la libreria en un archivo .c y otro .h
+ * -Se documenta mejor y se estandariza la forma de presentar los datos
+ * -Se corrige la funcion SetBrightness(CambiarBrillo) que no funcionaba ¿?
+ * -Se cambian nombres a algunas funciones y variables
+ * -Se crea la funcion LlenarDeColor
+ * -Se cambia la forma de usar el pin de datos
+ * 
+ * v0.2 (21/Abril/2014)
+ * -Mejor manejo de la memoria
+ * -Se añade posibilidad de  enviar datos a 400Khz o 800Khz
+ * 
+ * v0.1 (14/Abril/2014)
+ * -Primera version
+ * -------------------------------------------------------------------------- */
+
+#ifndef LED_PIXELS_H
+#define	LED_PIXELS_H
+
+/* COMPROBACIONES DE COMPATIBILIDAD */
+#if getenv("VERSION")<5.064
+	//Libreria creada con compilador CCS v5.064
+	//Para versiones anteriores comprobar que el codigo
+	//generado se adecua a los tiempos requeridos
+   #WARNING "Compilador antiguo, comprueba que el codigo generado es valido"
+#endif
+
+#if getenv("CLOCK") != 32000000
+	//Necesitamos que el PIC corra a 32MHZ
+	#ERROR "La velocidad del PIC debe ser de 32Mhz"
+#endif
+
+#ifndef PIX_NUM_LEDS
+	#ERROR "PIX_NUM_LEDS no definido"
+#else
+	#if PIX_NUM_LEDS > 85
+		//Solo podemos usar un maximo de 85 leds.
+		//Cada led usa 3 bytes, y nuestro contador de envio es un INT
+		#ERROR "PIX_NUM_LEDS tiene que ser menor a 85"
+	#elif (PIX_NUM_LEDS*3) > getenv("RAM")
+		#ERROR Tu PIC no tiene suficiente RAM para tantos LEDs
+	#endif
+#endif
+
+#ifndef PIX_PIN
+	#error "PIX_PIN no definido"
+#endif
+
+/* REGISTROS */
+#byte INDF0	= getenv("SFR:INDF0")
+#byte FSR0L	= getenv("SFR:FSR0L")
+#byte FSR0H	= getenv("SFR:FSR0H")
+#byte TMR2	= getenv("SFR:TMR2")
+#bit GIE	= getenv("BIT:GIE")
+#bit TMR2IF	= getenv("BIT:TMR2IF")
+
+/* DEFINES */
+#define PIX_NUM_BYTES	(PIX_NUM_LEDS * 3)
+
+/* COLORES BASICOS */
+//¡¡¡ Los colores cambian segun el voltaje que se aplique a los LEDs !!!
+#define PIX_NEGRO		0x000000	//negro
+#define PIX_ROJO		0xFF0000	//rojo
+#define PIX_NARANJA		0xFF6A00	//naranja
+#define PIX_AMARILLO	0x7D7D00	//amarillo
+#define PIX_VERDE		0x00FF00	//verde
+#define PIX_CELESTE		0x1284A5	//celeste
+#define PIX_AZUL		0x0000FF	//azul
+#define PIX_FUCSIA		0xFF005A	//fucsia
+#define PIX_VIOLETA		0xAC00FF	//violeta
+#define PIX_BLANCO		0xFFFFFF 	//blanco
+
+/* VARIABLES */
+int Pixels[PIX_NUM_BYTES];	//Array donde se guardan los valores de los leds
+int Brillo = 0;				//Permite ajustar el brillo sin modificar el color
+
+/* PROTOTIPOS */
+void InitPixels(void);
+void SetPixelColor(int n, int r, int g, int b);
+void SetPixelColor(int n, int32 c);
+int32 Color32(int r, int g, int b);
+void CambiarBrillo(int b);
+void LlenarDeColor(int from, int to, int32 c);
+void MostrarPixels(void);
+
+#endif	/* LED_PIXELS_H */
+
